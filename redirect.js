33d64aafa38d81804c3ffc7c96c8d9b1927d9869
James Hall <jamesbhall423@byui.edu>
2023-02-07 13:57:35 -0700

    redirect test 2

--- redirect.js
+++ redirect.js
@@ -1,7 +1,9 @@
 
 const redirect = (sub_url) => {
-    host_url = window.location.hostname;
+    href = window.location.href;
+    console.log(href);
+    host_url = href.substring(0,href.lastIndexOf("/"));
     console.log(host_url);
-    window.location.replace("/"+sub_url);
+    window.location.replace(host_url+"/"+sub_url);
 }
 redirect("activity-scheduler.html")
