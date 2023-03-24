860e740724eeaa6772d58c7728925097b9d8daf2
James Hall <jamesbhall423@byui.edu>
2023-03-24 17:27:02 -0600

    changes

--- firebase_config.js
+++ firebase_config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://jamesbhall423.github.io/',
+  url: 'https://jamesbhall423.github.io/Activity-Scheduler',
   // This must be true.
   handleCodeInApp: true,
   iOS: {
