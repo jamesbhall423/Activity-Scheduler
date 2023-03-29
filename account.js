5276b5735c978697a213d4049bce305d6723bc33
James Hall <jamesbhall423@byui.edu>
2023-03-29 17:56:21 -0600

    changes

--- account.js
+++ account.js
@@ -100,6 +100,40 @@ function continue_after_verification() {
 }
 function cancel_verification() {
     logout();
-    signIn.style.display = "grid";
-    verification.style.display = "none";
+    document.getElementById("signIn").style.display = "grid";
+    document.getElementById("verification").style.display = "none";
 }
+function cancel_email_link() {
+    window.localStorage.removeItem('emailForSignIn', email);
+    document.getElementById("signIn").style.display = "grid";
+    document.getElementById("email_link_waiting").style.display = "none";
+}
+// Confirm the link is a sign-in with email link.
+if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+    // Additional state parameters can also be passed via URL.
+    // This can be used to continue the user's intended action before triggering
+    // the sign-in operation.
+    // Get the email if available. This should be available if the user completes
+    // the flow on the same device where they started it.
+    var email = window.localStorage.getItem('emailForSignIn');
+    if (!email) {
+      // User opened the link on a different device. To prevent session fixation
+      // attacks, ask the user to provide the associated email again. For example:
+      email = window.prompt('Please provide your email for confirmation');
+    }
+    // The client SDK will parse the code from the link for you.
+    firebase.auth().signInWithEmailLink(email, window.location.href)
+      .then((result) => {
+        // Clear email from storage.
+        window.localStorage.removeItem('emailForSignIn');
+        // You can access the new user via result.user
+        // Additional user info profile not available via:
+        // result.additionalUserInfo.profile == null
+        // You can check if the user is new or existing:
+        // result.additionalUserInfo.isNewUser
+      })
+      .catch((error) => {
+        // Some error occurred, you can inspect the code: error.code
+        // Common errors could be invalid email and invalid or expired OTPs.
+      });
+  }
